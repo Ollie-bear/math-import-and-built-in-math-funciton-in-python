12acc9049dfc55f82047b2d0481f64a57be56f86
Ollie-bear <104463327+Ollie-bear@users.noreply.github.com>
2022-04-27 14:34:56 -0400

    Add files via upload

---
+++ math_import (1).py
@@ -0,0 +1,18 @@
+import math
+
+#The math.sqrt() method for example, returns the square root of a number:
+
+print(math.sqrt(64))
+
+#The math.ceil() method rounds a number upwards to its nearest integer, and the math.floor() method rounds a number downwards to its nearest integer, and returns the result:
+
+print(math.ceil(1.4))
+print(math.floor(1.4))
+
+#The math.pi constant, returns the value of PI (3.14...):
+
+print(math.pi)
+
+#Returns the base-10 logarithm of x
+
+print(math.log10(100))
