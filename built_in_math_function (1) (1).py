d682e48473b4328ac75eb6bc7bc752d31604adb9
Ollie-bear <104463327+Ollie-bear@users.noreply.github.com>
2022-04-27 14:35:44 -0400

    Add files via upload

---
+++ built_in_math_function (1) (1).py
@@ -0,0 +1,13 @@
+lst = [42, 52, 32, 52, 5, 47, 35, 72, 12, 6]
+
+#Minimum and maximum in a list function
+print(min(lst))
+print(max(lst))
+
+#Absolute value 
+
+print(abs(-7.25))
+
+#Exponent function 
+
+print(pow(4,3))
